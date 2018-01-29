2869528da70d1e4f97950bf70999af6cbf1c366d
stevewgh <stevewgh@users.noreply.github.com>
2018-01-29 21:10:06 +0000

    Added unit test. Internals visible to attribute.

---
+++ src/Topshelf.Leader.Tests/LeaderConfigurationBuilderShould.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Topshelf.Leader.Tests
+{
+    public class LeaderConfigurationBuilderShould
+    {
+        [Fact]
+        public void prevent_a_leader_configuration_which_doesnt_handle_starting_up()
+        {
+            var builder = new LeaderConfigurationBuilder<object>();
+            Assert.Throws<HostConfigurationException>(() => builder.Build());
+        }
+    }
+}
---
+++ src/Topshelf.Leader/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+[assembly:InternalsVisibleTo("Topshelf.Leader.Tests")]
